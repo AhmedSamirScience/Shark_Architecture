e6b21e885f3a6e1fd576b254647b2c3c108dc540
Ahmed <Ahmed@gmail>
2024-01-14 13:05:39 +0200

    added default dependencies in modularization - BuildSrc module - comments to describe each command - base libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,26 @@
+import dependencies.defaultLibraries
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    //plugins {} Block: Similar to the build.gradle/.kts in the root project, this applies plugins in this module.
+    // It doesn't need to explicitly call version() because it has already been specified in the root project.
+    // apply(true) is also not needed because by default it is true.
+
+    id(ProjectPlugins.androidApp) //plugin to build Android app (e.g. default app module) (AGP)
+    id(ProjectPlugins.kotlinAndroid) //plugin to enable Kotlin support in your project.
 }
 
 android {
-    namespace = "com.ascoding.shark_architecture"
-    compileSdk = 34
+    namespace = ProjectConfiguration.appId
+    compileSdk = ProjectConfiguration.compileSdk
 
     defaultConfig {
-        applicationId = "com.ascoding.shark_architecture"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        //defaultConfig {} Block -> This specifies the default configuration for the project.
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = ProjectConfiguration.appId
+        minSdk = ProjectConfiguration.minSdk
+        targetSdk = ProjectConfiguration.targetSdk
+        versionCode = ProjectConfiguration.versionCode
+        versionName = ProjectConfiguration.versionName
+        testInstrumentationRunner = ProjectConfiguration.testInstrumentationRunner
     }
 
     buildTypes {
@@ -27,21 +33,35 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        //compileOptions {} Block -> This specifies options related to compiling your Java code.
+        sourceCompatibility = JavaVersion.VERSION_17 //sourceCompatiblity -> specifies the Java version that your code uses.
+        targetCompatibility = JavaVersion.VERSION_17 //targetCompatiblity -> specifies the Java runtime version that your code will be executed on.
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        //kotlinOptions {} Block ->This specifies options related to compiling your Kotlin code.
+
+        //jvmTarget - specifies the Java Virtual Machine (JVM) version your code will be compiled for.
+        // In short, it compiles your code to byte code that is compatible with the JVM version that you specified.
+        //If you don't specify any compile / Kotlin options here, the default value will be used which could be different for a different version of the build Gradle plugin.
+        jvmTarget = ProjectConfiguration.jvmTarget
+
+    }
+
+    packaging {
+        //packagingOptions {} Block -> This specifies certain resources to exclude from the Android package - APK or Android bundle files.
+        resources {
+            //resources -> specifies the set of resources that should be processed during the packaging process
+
+            //it excludes the following license files: (*/META-INF/AL2.0) and (*/META-INF/LGPL2.1)
+            // The purpose is to reduce the Android package file size.
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    defaultLibraries()
+
+
 }
---
+++ buildSrc/src/main/kotlin/dependencies/DependencyHandler.kt
@@ -0,0 +1,25 @@
+package dependencies
+
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+fun DependencyHandler.implementation(dependency: String) {
+    add("implementation", dependency)
+}
+fun DependencyHandler.test(dependency: String) {
+    add("test", dependency)
+}
+fun DependencyHandler.androidTest(dependency: String) {
+    add("androidTest", dependency)
+}
+fun DependencyHandler.debugImplementation(dependency: String) {
+    add("debugImplementation", dependency)
+}
+fun DependencyHandler.kapt(dependency: String) {
+    add("kapt", dependency)
+}
+fun DependencyHandler.testImplementation(dependency: String) {
+    add("testImplementation", dependency)
+}
+fun DependencyHandler.androidTestImplementation(dependency: String) {
+    add("androidTestImplementation", dependency)
+}
