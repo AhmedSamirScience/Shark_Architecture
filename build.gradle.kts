df4bc93ad45bce47db29c21fb2c9527c040354f5
Ahmed <Ahmed@gmail>
2024-01-18 10:32:05 +0200

    added and finalise the rest of concepts -setting.gradle -dependencyHandler -build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+//plugins block {}
+//This defines the core plugins that are used by all your modules.
 plugins {
     //Android Gradle Plugin (AGP)
     //You can specify the Gradle version in either the File > Project Structure > Project menu in Android Studio
