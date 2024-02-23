fea1f212e24cf6fa87fd407ab018ed67c4eadb76
Ahmed <Ahmed@gmail>
2024-02-23 14:43:00 +0200

    ig

---
+++ buildSrc/src/main/kotlin/ProjectPlugins.kt
@@ -0,0 +1,9 @@
+object ProjectPlugins {
+    //plugin to build Android app (e.g. default app module)
+    const val androidApp = "com.android.application"
+    const val androidAppVersionNumber = "8.2.0"
+
+    //plugin to enable Kotlin support in your project.
+    const val kotlinAndroid = "org.jetbrains.kotlin.android"
+    const val kotlinAndroidPluginVersionNumber = "1.8.22"
+}
